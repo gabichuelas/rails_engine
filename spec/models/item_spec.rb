6b7bece0f7661f727d5ab3377d40faababb78f2f
Gaby Mendez <gmendez90@gmail.com>
2020-08-30 17:21:30 -0400

    Write Item spec for #to_dollars

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe Item do
   describe 'relationships' do
     it { should belong_to :merchant }
   end
+
+  describe 'before_create methods' do
+    it 'to_dollars' do
+      item_params = {
+        name: 'Item Qui Esse',
+        description: 'Nihil autem sit odio inventore deleniti. Est laudantium ratione distinctio laborum. Minus voluptatem nesciunt assumenda dicta voluptatum porro.',
+        unit_price: 75107,
+        merchant_id: 1,
+        created_at: '2012-03-27 14:53:59 UTC',
+        updated_at: '2012-03-27 14:53:59 UTC'
+      }
+
+      item = Item.create(item_params)
+      expect(item.unit_price).to eq(751.07)
+    end
+  end
 end
