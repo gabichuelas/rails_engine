28b57111e47d6327b61b8aef31e71282d1e92fa7
Gaby Mendez <gmendez90@gmail.com>
2020-08-30 23:15:25 -0400

    Add many to many relationships in model specs/defs

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,11 @@ class Invoice < ApplicationRecord
   validates_presence_of :customer_id
   validates_presence_of :merchant_id
   validates_presence_of :status
+
   belongs_to :customer
-  belongs_to :merchant 
+  belongs_to :merchant
+
+  has_many :transactions 
+  has_many :invoice_items
+  has_many :items, through: :invoice_items
 end
