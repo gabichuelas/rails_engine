6844e5dfa47750a62bb9cb873faf108b39e4b429
Gaby Mendez <gmendez90@gmail.com>
2020-08-31 20:52:14 -0400

    Write merchants#destroy spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe "Api::V1::Merchants", type: :request do
 
   it 'GET merchants#index' do
 
-    get "/api/v1/merchants"
+    get api_v1_merchants_path
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to have_http_status(:success)
@@ -17,11 +17,27 @@ RSpec.describe "Api::V1::Merchants", type: :request do
 
   it 'GET merchants#show' do
 
-    get "/api/v1/merchants/#{@merchant.id}"
+    get api_v1_merchant_path(@merchant.id)
     json = JSON.parse(response.body, symbolize_names: true)
 
     expect(response).to have_http_status(:success)
     expect(json[:data]).to have_key(:id)
     expect(json[:data][:attributes]).to have_key(:name)
+  end
+
+  it 'DELETE merchants#destroy' do
+
+    merchant_id = create(:merchant).id
+    get api_v1_merchant_path(merchant_id)
+    json1 = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json1[:data][:id].to_i).to eq(merchant_id)
+
+    delete api_v1_merchant_path(merchant_id)
+    json2 = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to have_http_status(:success)
+    # expect(json2[:data]).to have_key(:id)
+    # expect(json2[:data][:attributes]).to have_key(:name)
   end
 end
