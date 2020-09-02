8929ee546084972568fd758f36db0d128fc10de9
Gaby Mendez <gmendez90@gmail.com>
2020-09-02 15:45:56 -0400

    Write and pass spec to check for item finder on integers

--- spec/requests/api/v1/finder_spec.rb
+++ spec/requests/api/v1/finder_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe "Single Finders", type: :request do
   before :each do
     # "Whimsical Nightstand"
     create(:item)
-    create(:item, name: "Mid Mod Desk" )
-    create(:item, name: "Rustic Desk" )
+    create(:item, unit_price: 33.27, name: "Mid Mod Desk" )
+    create(:item, unit_price: 3.50, name: "Rustic Desk" )
     create(:item, name: "Desklite 2" )
 
     # Robin Dean Designs
@@ -14,8 +14,8 @@ RSpec.describe "Single Finders", type: :request do
     @headers = { "CONTENT_TYPE" => "application/json" }
   end
 
-  describe 'Find Items: returns single record that matches given criteria' do
-    it 'can Find for name attribute' do
+  describe 'Find Item: returns single record that matches given criteria' do
+    it 'can Find for name (string) attribute' do
       get "/api/v1/items/find?name=desk"
       item = JSON.parse(response.body, symbolize_names: true)
 
@@ -27,6 +27,14 @@ RSpec.describe "Single Finders", type: :request do
 
       expect(item.count).to eq(1)
       expect(item[:data][:attributes][:name].downcase).to include('dEsk'.downcase)
+    end
+
+    it 'can Find for unit_price (integer) attribute' do
+      get "/api/v1/items/find?unit_price=3"
+      item = JSON.parse(response.body, symbolize_names: true)
+
+      expect(item.count).to eq(1)
+      expect(item[:data][:attributes][:unit_price].to_s).to include("3")
     end
   end
 
