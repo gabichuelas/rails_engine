bb69cf4ddb4050ed7c5988fcec8b68fda120ebad
Gaby Mendez <gmendez90@gmail.com>
2020-09-01 22:21:41 -0400

    Write spec for find_all - name

--- spec/requests/api/v1/single_finders_spec.rb
+++ spec/requests/api/v1/single_finders_spec.rb
@@ -14,24 +14,40 @@ RSpec.describe "Single Finders", type: :request do
     @headers = { "CONTENT_TYPE" => "application/json" }
   end
 
-  it 'Find Items: returns single record that matches given criteria' do
+  describe 'Find Items: returns single record that matches given criteria' do
+    it 'can Find for name attribute' do
+      get "/api/v1/items/find?name=desk"
+      item = JSON.parse(response.body, symbolize_names: true)
 
-    get "/api/v1/items/find?name=desk"
-    item = JSON.parse(response.body, symbolize_names: true)
+      expect(item.count).to eq(1)
+      expect(item[:data][:attributes][:name].downcase).to include('desk')
 
-    expect(item.count).to eq(1)
-    expect(item[:data][:attributes][:name].downcase).to include('desk')
+      get "/api/v1/items/find?name=dEsk"
+      item = JSON.parse(response.body, symbolize_names: true)
 
-    get "/api/v1/items/find?name=dEsk"
-    item = JSON.parse(response.body, symbolize_names: true)
-
-    expect(item.count).to eq(1)
-    expect(item[:data][:attributes][:name].downcase).to include('dEsk'.downcase)
+      expect(item.count).to eq(1)
+      expect(item[:data][:attributes][:name].downcase).to include('dEsk'.downcase)
+    end
   end
 
-  xit 'Find All Items: returns all records that matches given criteria' do
+  describe 'Find All Items: returns all records that matches given criteria' do
+    it 'can Find_all for name attribute' do
+      get "/api/v1/items/find_all?name=desk"
+      items = JSON.parse(response.body, symbolize_names: true)
 
-    get "/api/v1/items/find_all?name=desk"
+      expect(items.count).to eq(3)
+      items.each do |item|
+        expect(item[:data][:attributes][:name].downcase).to include('desk')
+      end
 
+      get "/api/v1/items/find_all?name=dEsk"
+
+      items = JSON.parse(response.body, symbolize_names: true)
+
+      expect(items.count).to eq(3)
+      items.each do |item|
+        expect(item[:data][:attributes][:name].downcase).to include('desk'.downcase)
+      end
+    end
   end
 end
