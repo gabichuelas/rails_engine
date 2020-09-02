f0c9d32731ce635514d5b727aad6a853dcd0309b
Gaby Mendez <gmendez90@gmail.com>
2020-09-01 22:17:07 -0400

    Add strong params to Item::SearchController

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -2,10 +2,16 @@ class Api::V1::Items::SearchController < ApplicationController
   def show
     # require "pry"; binding.pry
     # do query params always show up first in the params hash?
-    attribute = params.keys.first
-    search_value = params[attribute].downcase
+    attribute = item_params.keys.first
+    search_value = item_params[attribute].downcase
     # find Item where attribute: includes search_value
     items = Item.where("items.#{attribute} ILIKE ?", "%#{search_value}%")
     render json: ItemSerializer.new(items.first)
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id, :created_at, :updated_at)
   end
 end
