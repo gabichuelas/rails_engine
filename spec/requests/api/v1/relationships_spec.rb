c01c989da76a7c4011acab1c9d1fe72bccc275e4
Gaby Mendez <gmendez90@gmail.com>
2020-09-01 20:03:11 -0400

    Write spec for merchant/item relationships

---
+++ spec/requests/api/v1/relationships_spec.rb
@@ -0,0 +1,29 @@
+RSpec.describe "Api::V1 Item/Merchant relationships", type: :request do
+  before :each do
+    @merchant = create(:merchant)
+    3.times { create(:item, merchant: @merchant) }
+    @headers = { "CONTENT_TYPE" => "application/json" }
+  end
+
+  describe 'GET /api/v1/merchants/:id/items' do
+    it 'returns all items associated with a merchant' do
+
+      get "/api/v1/merchants/#{@merchant.id}/items"
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response).to have_http_status(:success)
+      expect(json[:data].count).to eq(3)
+
+      json[:data].each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:attributes][:merchant_id]).to eq(@merchant.id)
+      end
+    end
+  end
+
+  describe 'GET /api/v1/items/:id/merchant' do
+    xit 'returns the merchant associated with an item' do
+
+    end
+  end
+end
