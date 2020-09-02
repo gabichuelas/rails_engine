11a21af5ea017029678b70988f56d29bbf07688d
Gaby Mendez <gmendez90@gmail.com>
2020-09-01 20:40:46 -0400

    Write spec for items - single finder

---
+++ spec/requests/api/v1/single_finders_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe "Single Finders", type: :request do
+  before :each do
+    # "Whimsical Nightstand"
+    create(:item)
+    create(:item, name: "Mid Mod Desk" )
+    create(:item, name: "Rustic Desk" )
+    create(:item, name: "Desklite 2" )
+
+    # Robin Dean Designs
+    create(:merchant)
+    create(:merchant, name: "Bikes & More")
+    create(:merchant, name: "Atlantic Designs")
+
+    @headers = { "CONTENT_TYPE" => "application/json" }
+  end
+
+  it 'Find Items: returns single record that matches given criteria' do
+
+    get "/api/v1/items/find?name=desk"
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(item.count).to eq(1)
+    expect(item[:data][:attributes][:name]).to include('desk')
+  end
+
+  xit 'Find All Items: returns all records that matches given criteria' do
+
+    get "/api/v1/items/find_all?name=desk"
+
+  end
+end
