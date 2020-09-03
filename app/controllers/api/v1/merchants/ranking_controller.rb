dd873e0a90af3882d3ac18f84cd0205d47aed5c8
Gaby Mendez <gmendez90@gmail.com>
2020-09-02 22:04:43 -0400

    Write rankingController#most_revenue

--- app/controllers/api/v1/merchants/ranking_controller.rb
+++ app/controllers/api/v1/merchants/ranking_controller.rb
@@ -5,5 +5,7 @@ class Api::V1::Merchants::RankingController < ApplicationController
   end
 
   def most_revenue
+    merchants = Merchant.rank_by_revenue(params[:quantity])
+    render json: MerchantSerializer.new(merchants)
   end
 end
