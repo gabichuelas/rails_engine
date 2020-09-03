1adf530c2bf31f4b03e3bfa846b46aa896d102a1
Gaby Mendez <gmendez90@gmail.com>
2020-09-02 23:24:06 -0400

    Refactor Payment success where methods to use merge and a Payment.success scope

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,15 +12,15 @@ class Merchant < ApplicationRecord
   }
 
   scope :rank_by_items_sold, -> (result_length) {
-    joins(invoices: [:invoice_items, :payments]).where("payments.result = 'success'").group(:id).order('sum(invoice_items.quantity) desc').limit(result_length)
+    joins(invoices: [:invoice_items, :payments]).merge(Payment.success).group(:id).order('sum(invoice_items.quantity) desc').limit(result_length)
   }
 
   scope :rank_by_revenue, -> (result_length) {
-    joins(invoices: [:invoice_items, :payments]).where("payments.result = 'success'").group(:id).order("sum(invoice_items.unit_price * invoice_items.quantity) desc").limit(result_length)
+    joins(invoices: [:invoice_items, :payments]).merge(Payment.success).group(:id).order("sum(invoice_items.unit_price * invoice_items.quantity) desc").limit(result_length)
   }
 
   def total_revenue
-    total = invoice_items.joins(:payments, :invoice).where("payments.result = 'success'").sum("quantity * unit_price")
+    total = invoice_items.joins(:payments, :invoice).merge(Payment.success).sum("quantity * unit_price")
     Revenue.new(total)
   end
 end
