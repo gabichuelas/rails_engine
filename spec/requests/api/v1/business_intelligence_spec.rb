bd5ac86dc088568089a5380cd2acbf1cc5afc9cb
Gaby Mendez <gmendez90@gmail.com>
2020-09-02 22:00:46 -0400

    Write spec for total_revenue for single merchant

--- spec/requests/api/v1/business_intelligence_spec.rb
+++ spec/requests/api/v1/business_intelligence_spec.rb
@@ -2,11 +2,11 @@ RSpec.describe 'Business Intelligence Endpoints' do
 
   before :each do
     merchantA = create(:merchant, name: "Merchant A")
-    merchantB = create(:merchant, name: "Merchant B")
+    @merchantB = create(:merchant, name: "Merchant B")
     merchantC = create(:merchant, name: "Merchant C")
 
     invoiceA = create(:invoice, merchant: merchantA)
-    invoiceB = create(:invoice, merchant: merchantB)
+    invoiceB = create(:invoice, merchant: @merchantB)
     invoiceC = create(:invoice, merchant: merchantC)
 
     create(:payment, invoice: invoiceA)
@@ -31,6 +31,21 @@ RSpec.describe 'Business Intelligence Endpoints' do
   end
 
   it 'can get revenue for a single merchant' do
-    # revenue = invoice_items.quantity x unit_price
+    # GET /api/v1/merchants/:id/revenue
+    get "/api/v1/merchants/#{@merchantB.id}/revenue"
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant[:data][:attributes][:revenue]).to eq(1400.90)
+  end
+
+  it 'can get revenue across date range' do
+    # GET /api/v1/revenue?start=<start_date>&end=<end_date>
+    get '/api/v1/revenue?start=2012-03-09&end=2012-03-24'
+  end
+
+  it 'can get merchants with the most revenue' do
+    # GET /api/v1/merchants/most_revenue?quantity=x
+    # AR query done, need test.
+
   end
 end
