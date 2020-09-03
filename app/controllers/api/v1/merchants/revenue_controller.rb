002baa881681064337d84cc230d33f93187cdea7
Gaby Mendez <gmendez90@gmail.com>
2020-09-02 22:46:38 -0400

    Create merchants::revenueController#show

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
-    # for a single merchant
     merchant = Merchant.find(params[:id])
     render json: RevenueSerializer.new(merchant.total_revenue)
   end
