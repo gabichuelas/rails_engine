445f88daeb9e1e3b01bc43a0a191c91e70aac2a3
Gaby Mendez <gmendez90@gmail.com>
2020-09-02 22:47:06 -0400

    Create revenueController#index

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::RevenueController < ApplicationController
   def index
     # revenue across date range
+    start_date = filter_params[:start]
+    end_date = filter_params[:end]
+    render json: RevenueSerializer.new(Merchant.revenue_for_range(start_date, end_date))
+  end
+
+  private
+
+  def filter_params
+    params.permit(:start, :end)
   end
 end
