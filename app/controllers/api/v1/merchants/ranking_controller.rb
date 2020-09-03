bf1273b167b9aab024f0d70c09b3d53421dad4c2
Gaby Mendez <gmendez90@gmail.com>
2020-09-02 20:14:31 -0400

    Write rankingController#most_items_sold

--- app/controllers/api/v1/merchants/ranking_controller.rb
+++ app/controllers/api/v1/merchants/ranking_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::Merchants::RankingController < ApplicationController
   def most_items_sold
-    require "pry"; binding.pry
     result_length = params[:quantity]
+    merchants = Merchant.joins(:invoice_items).joins(:payments).select('merchants.*, sum(invoice_items.quantity) as items_sold').where("payments.result = 'success'").group(:id).order('sum(invoice_items.quantity) desc').limit(result_length)
+    render json: MerchantSerializer.new(merchants)
   end
 
   def most_revenue
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,6 +4,9 @@ class Merchant < ApplicationRecord
   has_many :items, dependent: :destroy
   has_many :invoices, dependent: :destroy
 
+  has_many :invoice_items, through: :invoices
+  has_many :payments, through: :invoices
+
   scope :find_by_attribute, -> (attribute, value) {
     where("merchants.#{attribute}::text ILIKE ?", "%#{value}%")
   }
